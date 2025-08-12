b9fdc542e0ba4227e38c03a8ffc2675b5182de6c
Nitin tak <129604612+nitintak13@users.noreply.github.com>
2025-08-12 12:16:58 +0530

    Update multer.js

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,6 +1,11 @@
+// import multer from "multer";
+
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
+
+// export default upload;
+
 import multer from "multer";
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
+const upload = multer({ dest: "uploads/" });
 export default upload;
+
